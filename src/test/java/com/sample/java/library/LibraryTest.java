8880fe5ee848b44b04b8b64327b4c0c8c22db2aa
Szczepan Faber <sfaber@linkedin.com>
2020-03-24 09:03:43 -0700

    Added failing scenario
    Renaming a library method is not a compatible change

--- src/test/java/com/sample/java/library/LibraryTest.java
+++ src/test/java/com/sample/java/library/LibraryTest.java
@@ -8,6 +8,6 @@ import org.junit.Test;
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-        Assert.assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        Assert.assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod2());
     }
 }
